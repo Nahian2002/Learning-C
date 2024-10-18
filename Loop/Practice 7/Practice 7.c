614a7d3b3fd72557747a70fd19b7122f732eab87
Jayed Al Nahian <113072310+Nahian2002@users.noreply.github.com>
2024-10-18 18:41:47 +0600

    Practice

---
+++ Loop/Practice 7/Practice 7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int sum=0;
+
+    for(int i=0;i<=100;i++)
+
+    sum=sum+i;
+
+    printf("The sum of first 100 natural number is %d",sum);
+
+    return 0;
+}
