61401e28af3ac0b45302aeabc2a95258ccd43c07
Jayed Al Nahian <113072310+Nahian2002@users.noreply.github.com>
2024-10-21 19:01:33 +0600

    Add files via upload

---
+++ Loop/Practice 9/Practice 9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+
+    printf("Input a number: ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=10;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            printf("%2d x%2d = %2d  ",j,i,(j*i));
+        }
+        printf(" \n");
+    }
+    return 0;
+}
